b80e1b1da49304c13dd859e2587a2f40903f5886
mmihova <mmihova@MMIHOVA>
2022-03-29 00:03:10 +0300

    Add project files.

---
+++ Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MMUni.Models
+{
+    public class Course
+    {
+        public Course()
+        {
+            this.Students = new List<Student>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MMUni.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+            this.Courses = new List<Course>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Town { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public ICollection<Course> Courses { get; set; }
+
+    }
+}
